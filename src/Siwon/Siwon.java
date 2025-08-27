e86bd10ed49c48a76c5f416789b20489f95f2706
user <user@NucBox_M6>
2025-08-27 11:03:02 +0900

    인스타 물어볼껄..

--- src/Siwon/Siwon.java
+++ src/Siwon/Siwon.java
@@ -1,7 +1,7 @@
 package Siwon;
 
 public class Siwon {
-	static void main(String[] agrs) {
+	public static void main(String[] agrs) {
 
 		System.out.println("요로시쿠 오네가이시마스");
 		System.out.println("my name is siwon kim");
@@ -10,5 +10,7 @@ public class Siwon {
 		//막 수정해 막 수정해-250827_최우석
 
 		System.out.println("개어렵노");
+		System.out.println("시호쌤 개귀엽다 ㅇㅈ?");
+		
 	}
 }
